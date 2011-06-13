a48da4a20974ccca38312dc054a5bc2f5cb88e8a
Noam <noam@fiverr.com>
2011-06-13 17:36:39 +0300

    modified log prefix

--- lib/rack/contrib/deflect.rb
+++ lib/rack/contrib/deflect.rb
@@ -53,7 +53,7 @@ module Rack
       @local_storage_map = {}
       @app, @options = app, {
         :log => false,
-        :log_format => "deflect (#{@redis_storage.nil? ? "Local" : "Redis"})  %s  %s",
+        :log_format => "",
         :log_date_format => '%m/%d/%Y %H:%M:%S',
         :request_threshold => 100,
         :interval => 5,
@@ -71,6 +71,10 @@ module Rack
         @redis_storage = Redis.new(@options[:redis_connection_params]) unless @options[:redis_connection_params].blank?
       rescue Timeout::Error
         # No redis.
+      end
+      
+      if (@options[:log].blank?)
+        @options[:log] = "Deflect (#{@redis_storage.nil? ? "Local" : "Redis"})  %s  %s"
       end
       
       if @options[:fresh_start] == true && @redis_storage.present? 
